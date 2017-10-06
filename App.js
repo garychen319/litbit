d6370fc3c849956e13c4314010f17504dd493ecc
Jake Kwon <jhnana0629@gmail.com>
2017-10-06 15:40:42 -0400

    onboarding screen frontend

--- App.js
+++ App.js
@@ -1,13 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Picker, TextInput, Button, StyleSheet, Text, View } from 'react-native';
+import OnboardingScreen from "./src/OnboardingScreen.js"
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      text: "",
+    }
+    this.onPress = this.onPress.bind(this)
+
+    // firebase.database().ref('/counter').once('value').then(snapshot =>
+    //   this.setState({
+    //     count: snapshot.val().count
+    //   })
+    // );
+  }
+
+  onPress() {
+    // var nextCount = this.state.count + 1
+    // this.setState({
+    //   count: nextCount,
+    // })
+    // firebase.database().ref('counter').set({
+    //   count: nextCount,
+    // });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Picker
+          style={styles.picker} 
+          selectedValue={this.state.language}
+          onValueChange={(itemValue, itemIndex) => this.setState({language: itemValue})}>
+          <Picker.Item label="EastCampus" value="East Campus" />
+          <Picker.Item label="Ruggles" value="Ruggles" />
+          <Picker.Item label="Hogan" value="Ruggles" />
+        </Picker>
+        <Text style={{}}>Room number: 
+          <TextInput
+            style={{height: 30, width: 150, borderColor: 'gray', borderWidth: 1}}
+            onChangeText={(text) => this.setState({text})}
+            value={this.state.text}
+          />
+        </Text>
+        <Button
+          onPress={this.onPress}
+          title="submit"
+          color="#841584"
+        />
       </View>
     );
   }
@@ -19,5 +61,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  picker: {
+    width: 150,
   },
 });
--- src/OnboardingScreen.js
+++ src/OnboardingScreen.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
 
 export default class OnboardingScreen extends React.Component {
   render() {
     return (
-      <View/>
+    	<View>
+    		<Text> hey </Text>
+      	</View>
     )
   }
 }
