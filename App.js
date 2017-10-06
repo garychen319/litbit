27e2e73d2a90ec6d0c5235bb3c0d62fc7f852e66
Jake Kwon <jhnana0629@gmail.com>
2017-10-06 15:53:28 -0400

    onboarding first draft

--- App.js
+++ App.js
@@ -3,53 +3,10 @@ import { Picker, TextInput, Button, StyleSheet, Text, View } from 'react-native'
 import OnboardingScreen from "./src/OnboardingScreen.js"
 
 export default class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      text: "",
-    }
-    this.onPress = this.onPress.bind(this)
-
-    // firebase.database().ref('/counter').once('value').then(snapshot =>
-    //   this.setState({
-    //     count: snapshot.val().count
-    //   })
-    // );
-  }
-
-  onPress() {
-    // var nextCount = this.state.count + 1
-    // this.setState({
-    //   count: nextCount,
-    // })
-    // firebase.database().ref('counter').set({
-    //   count: nextCount,
-    // });
-  }
-
   render() {
     return (
       <View style={styles.container}>
-        <Picker
-          style={styles.picker} 
-          selectedValue={this.state.language}
-          onValueChange={(itemValue, itemIndex) => this.setState({language: itemValue})}>
-          <Picker.Item label="EastCampus" value="East Campus" />
-          <Picker.Item label="Ruggles" value="Ruggles" />
-          <Picker.Item label="Hogan" value="Ruggles" />
-        </Picker>
-        <Text style={{}}>Room number: 
-          <TextInput
-            style={{height: 30, width: 150, borderColor: 'gray', borderWidth: 1}}
-            onChangeText={(text) => this.setState({text})}
-            value={this.state.text}
-          />
-        </Text>
-        <Button
-          onPress={this.onPress}
-          title="submit"
-          color="#841584"
-        />
+        <OnboardingScreen/>
       </View>
     );
   }
