612c262fcc85c5329495bcb72428fca1d3388a97
David Lee <jl4397@columbia.edu>
2017-10-22 21:59:26 -0400

    Login successful

--- App.js
+++ App.js
@@ -2,28 +2,26 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './src/LoginScreen.js';
 import * as firebase from 'firebase';
+import {StackNavigator} from 'react-navigation';
 
-import firebaseConfig from './config/secrets.js';
-var firebaseApp = firebase.initializeApp(firebaseConfig)
+import secrets from './config/secrets.js';
+var firebaseApp = firebase.initializeApp(secrets.firebaseConfig)
+
+const Navigator = StackNavigator({
+  Login: { screen: LoginScreen }
+});
+
+// Listen for authentication state to change.
+firebase.auth().onAuthStateChanged((user) => {
+  if (user != null) {
+    console.log("We are authenticated now!");
+  }
+});
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <LoginScreen></LoginScreen>
-      </View>
+      <Navigator/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  picker: {
-    width: 150,
-  },
-});
